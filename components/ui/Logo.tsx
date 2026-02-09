f55f35c715dcc4565086452294120b929ba4bdc2
edgeofvision0218-ops <edgeofvision0218@gmail.com>
2026-02-09 16:21:25 +0100

    feat: update main logo to svg file

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 
 interface LogoProps {
     className?: string
@@ -8,17 +7,16 @@ interface LogoProps {
 
 export default function Logo({
     className = '',
-    logoPath = '/images/main-logo.png',
+    logoPath = '/images/mainLogo.svg',
 }: LogoProps) {
     return (
         <div className={`flex items-center gap-2 w-[180px] max-w-full min-w-0 shrink ${className}`}>
             <div className="relative w-full flex justify-center items-center aspect-[180/40] min-h-[40px]">
-                <Image
+                <img
                     src={logoPath}
                     alt="Scandi Commerce Logo"
                     width={180}
                     height={40}
-                    priority
                     className="w-full h-auto object-contain"
                 />
             </div>
